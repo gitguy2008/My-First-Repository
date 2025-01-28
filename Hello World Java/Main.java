6cd69d019545be14735a01c238cb64f5d90a5ef7
gitguy2008 <akotoyan08@gmail.com>
2025-01-28 14:07:53 -0800

    Add files via upload

---
+++ Hello World Java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Name totallyRadName1 = new Name("Connor", "Hacker90");
+        Name totallyRadName2 = new Name("Cheesy", "Sock");
+        System.out.println("Hello "+totallyRadName1.getFullName()+"!");
+        System.out.println("Hello "+totallyRadName2.getFullName()+"!");
+
+    }
+}
---
+++ Hello World Java/Name.java
@@ -0,0 +1,11 @@
+public class Name {
+    private String firstName;
+    private String lastName;
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
