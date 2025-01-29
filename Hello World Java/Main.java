da935c377acfccbc0816d8bf879158a00d826a70
Anthony Kotoyan <kotoyan.anthony@gmail.com>
2025-01-29 11:25:37 -0800

    Update Main.java

--- Hello World Java/Main.java
+++ Hello World Java/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalTime;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,10 +7,43 @@ public class Main {
         Name totallyRadName2 = new Name("Cheesy", "Sock");
         Name totallyRadName3 = new Name("Parham", "Rostami");
         
-        
-        System.out.println("Hello "+totallyRadName1.getFullName()+"!");
-        System.out.println("Hello "+totallyRadName2.getFullName()+"!");
-        System.out.println("Hello "+totallyRadName3.getFullName()+"!");
+        greetUser(totallyRadName1);
+        greetUser(totallyRadName2);
+        greetUser(totallyRadName3);
+    }
 
+    private static void greetUser(Name user) {
+        String greeting = getGreetingMessage();
+        System.out.println(greeting + " " + user.getFullName() + "!");
+    }
+
+    private static String getGreetingMessage() {
+        LocalTime time = LocalTime.now();
+        int hour = time.getHour();
+
+        if (hour >= 5 && hour < 12) {
+            return "Good Morning";
+        } else if (hour >= 12 && hour < 17) {
+            return "Good Afternoon";
+        } else if (hour >= 17 && hour < 21) {
+            return "Good Evening";
+        } else {
+            return "Hello";
+        }
+    }
+
+    // Assuming the Name class looks something like this
+    static class Name {
+        private String firstName;
+        private String nickname;
+
+        public Name(String firstName, String nickname) {
+            this.firstName = firstName;
+            this.nickname = nickname;
+        }
+
+        public String getFullName() {
+            return firstName + " \"" + nickname + "\"";
+        }
     }
 }
