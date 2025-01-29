d2fef8fb0ee50a5d3bf3b3a763e00de8c75ab77b
gitguy2008 <akotoyan08@gmail.com>
2025-01-29 10:47:27 -0800

    Update Main.java

--- Hello World Java/Main.java
+++ Hello World Java/Main.java
@@ -3,8 +3,12 @@ public class Main {
     public static void main(String[] args) {
         Name totallyRadName1 = new Name("Connor", "Hacker90");
         Name totallyRadName2 = new Name("Cheesy", "Sock");
+        Name totallyRadName3 = new Name("Parham", "Rostami");
+        
+        
         System.out.println("Hello "+totallyRadName1.getFullName()+"!");
         System.out.println("Hello "+totallyRadName2.getFullName()+"!");
+        System.out.println("Hello "+totallyRadName3.getFullName()+"!");
 
     }
 }
